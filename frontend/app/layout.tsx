8dc4bc15a22a676fa990ba50f23416dff3a41c92
ywe <youngwook4146@gmail.com>
2024-03-11 19:19:34 +0900

    Added scroll stop

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+
+import Header from "./component/Header"
+import Footer from "./component/Footer"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-[#282828] overscroll-none">
+        <Header/> 
+
+        {children}
+        
+        <Footer/>
+      </body>
     </html>
   );
 }
