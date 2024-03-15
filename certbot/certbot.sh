bf840b90a526c27b57889538c5d4aefc8aaf19e6
ywe <youngwook4146@gmail.com>
2024-03-15 17:19:20 +0900

    Fixed typos

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,1 +1,1 @@
-docker-compowe run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d example.org
+docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d example.org
